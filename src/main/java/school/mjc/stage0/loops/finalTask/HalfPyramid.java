398c40076bb58ae57937a2af55d460cdc6e2920c
aliaksei-andreyeu <aliaksei.andreyeu@gmail.com>
2023-11-20 19:52:35 +0300

    Submit Module 4.2 Final Task

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        for (int x = 1; x <= cathetusLength; x++) {
+            for (int y = cathetusLength - x; y >= 1; y--) {
+                System.out.print(" ");
+            }
+            for (int y = x; y > 0; y--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int x = 1; x <= cathetusLength; x++) {
+            for (int y = cathetusLength - x; y >= 1; y--) {
+                System.out.print(" ");
+            }
+            for (int y = x; y > 1; y--) {
+                System.out.print(y);
+            }
+            for (int y = 1; y <= x; y++) {
+                System.out.print(y);
+            }
+            System.out.println();
+        }
 
     }
 
